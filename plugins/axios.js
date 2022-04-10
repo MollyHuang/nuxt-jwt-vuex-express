31629d5e978ccabae9f191f94f18834f1e6e9387
Molly Huang <molly.h@gmail.com>
2022-04-10 15:29:50 +0800

    Refresh Tokens - 修改 plugins/axios.js 添加錯誤處理程序以在access token過期時自動嘗試刷新它。 如果access token過期，API 需要通知客戶端token不再有效，需要刷新。 這通常通過返回 401 狀態代碼和某種描述性有效的payload來完成。 expired-token-payload.json {   "status": "failed",   "text_code": "TOKEN_EXPIRED",   "message": "The JWT token is expired",   "status_code": 401 }

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,10 +3,16 @@
 // expose the store, axios client and redirect method from the Nuxt context
 // https://nuxtjs.org/api/context/
 export default function ({ store, app: { $axios }, redirect }) {
+  console.log('[plugins/axios.js][begin] store.state =', store.state)
+  // console.log('[plugins/axios.js][begin] $axios =', $axios)
+  console.log('[plugins/axios.js][begin] redirect =', redirect)
+
   const IGNORED_PATHS = ['/auth/login', '/auth/logout', '/auth/refresh']
 
   $axios.onRequest((config) => {
     // check if the user is authenticated
+    console.log('[plugins/axios.js][onRequest][begin] config =', config)
+    // console.log('[plugins/axios.js][onRequest][begin] store.state.auth =', store.state.auth)
     if (store.state.auth.access_token) {
       // set the Authorization header using the access token
       config.headers.Authorization = 'Bearer ' + store.state.auth.access_token
@@ -16,12 +22,17 @@ export default function ({ store, app: { $axios }, redirect }) {
   })
 
   $axios.onError((error) => {
+    // console.log('[plugins/axios.js][onError][begin] store.state =', store.state)
+    console.log('[plugins/axios.js][onError][begin] error =', error)
     return new Promise(async (resolve, reject) => {
+
       // ignore certain paths (i.e. paths relating to authentication)
       const isIgnored = IGNORED_PATHS.some(path => error.config.url.includes(path))
+      console.log('[plugins/axios.js][onError] isIgnored =', isIgnored)
 
       // get the status code from the response
       const statusCode = error.response ? error.response.status : -1
+      console.log('[plugins/axios.js][onError] statusCode =', statusCode)
 
       // only handle authentication errors or errors involving the validity of the token
       if ((statusCode === 401 || statusCode === 422) && !isIgnored) {
@@ -37,24 +48,29 @@ export default function ({ store, app: { $axios }, redirect }) {
 
         // retrieve the text_code property from the response, or default to null
         const { data: { text_code } = { text_code: null } } = error.response || {}
+        console.log('[plugins/axios.js][onError][401,402,!isIgnored] text_code =', text_code)
 
         // get the refresh token from the state if it exists
         const refreshToken = store.state.auth.refresh_token
+        console.log('[plugins/axios.js][onError][401,402,!isIgnored] refreshToken =', refreshToken)
 
         // determine if the error is a result of an expired access token
         // also ensure that the refresh token is present
         if (text_code === 'TOKEN_EXPIRED' && refreshToken) {
 
           // see below - consider the refresh process failed if this is a 2nd attempt at the request
+          console.log('[plugins/axios.js][onError][TOKEN_EXPIRED] error.config =', error.config)
           if (error.config.hasOwnProperty('retryAttempts')) {
             // immediately logout if already attempted refresh
             await store.dispatch('auth/logout')
 
             // redirect the user home
             return redirect('/')
-          } else {
+          }
+          else {
             // merge a new retryAttempts property into the original request config to prevent infinite-loop if refresh fails
             const config = { retryAttempts: 1, ...error.config }
+            console.log('[plugins/axios.js][onError][TOKEN_EXPIRED][else] config =', config)
 
             try {
               // attempt to refresh access token using refresh token
@@ -71,7 +87,10 @@ export default function ({ store, app: { $axios }, redirect }) {
               return redirect('/')
             }
           }
-        } else if (text_code === 'TOKEN_INVALID') {
+        }
+        else if (text_code === 'TOKEN_INVALID') {
+          console.log('[plugins/axios.js][onError][TOKEN_INVALID] text_code =', text_code)
+
           // catch any other JWT-related error (i.e. malformed token) and logout the user
           await store.dispatch('auth/logout')
 
